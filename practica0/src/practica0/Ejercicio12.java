2466cadb78e990463016ef2b9824c56116e9a8a2
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-13 20:53:53 -0300

    se a agregado el ejercicio 12

---
+++ practica0/src/practica0/Ejercicio12.java
@@ -0,0 +1,14 @@
+package practica0;
+
+//Escribir un método static double potencia(double x, int a) que toma un número racional
+//x y un entero a y calcula x elevado a "a".
+
+
+public class Ejercicio12 {
+
+	
+	public static double potencia(double x, int a) {
+		return Math.pow(x, a);
+	}
+
+}
