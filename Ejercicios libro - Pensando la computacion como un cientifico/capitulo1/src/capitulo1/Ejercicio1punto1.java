a9125714703f2ebefc3a1cc845ad0e06f6a707ad
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-21 23:20:33 -0300

    Se agregó el ejercicio1.1 del capítulo 1 del libro

---
+++ Ejercicios libro - Pensando la computacion como un cientifico/capitulo1/src/capitulo1/Ejercicio1punto1.java
@@ -0,0 +1,33 @@
+package capitulo1;
+
+public class Ejercicio1punto1 {
+
+//	Los científicos de la computación tienen el hábito molesto de usar palabras
+//	del castellano para referirse a cosas que no tienen nada que ver con su significado
+//	más común. Por ejemplo, en castellano, una sentencia es la disposición de un juez,
+//	mientras que en programación, es básicamente una instrucción.
+//	El glosario al final de cada capítulo pretende resaltar palabras y frases que
+//	tienen un significado especial en las ciencias de la computación. Cuando veas palabras 
+	//conocidas en el contexto de la programación, ¡no asumas que sabés su significado!
+//	a. ¿Qué es una sentencia? ¿Qué es un comentario?
+	
+//	Una sentencia es una parte de un programa que especifica una acción que se efectuará 
+//	cuando el programa se ejecute. Una sentencia print, por ejemplo, hace que se muestre
+//	un valor por pantalla.
+//	Un cometario es una parte del programa que contiene información para ser leida por 
+//	una persona, pero que no tiene un efecto en la ejecución del programa.
+//	
+//	b. ¿Qué quiere decir que un programa sea portable?
+	
+//	Un programa es portable cuando puede ser ejecutado en más de un tipo de computadora.
+	
+//	c. ¿Qué es compilar un programa? ¿A qué nos referimos cuando decimos que
+//	un lenguaje es intepretado?
+	
+//	Compilar un programa es traducir un programa escrito en un lenguaje de alto nivel
+//	a un lenguaje de alto nivel de una vez, en participación para al ejecución posterior.
+//	Un lenguaje es interpretado cuando es al ejecutar un programa escrito en un lenguaje
+//	de alto nivel traduciéndolo línea por línea.
+
+
+}
