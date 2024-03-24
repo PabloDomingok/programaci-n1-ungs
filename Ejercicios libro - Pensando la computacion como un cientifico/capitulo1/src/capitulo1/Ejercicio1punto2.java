50ff5a23cf501f635f9aa18351732316402d01b2
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-24 13:41:28 -0300

    Se ha agregado el ejercicio1.2 del libro

---
+++ Ejercicios libro - Pensando la computacion como un cientifico/capitulo1/src/capitulo1/Ejercicio1punto2.java
@@ -0,0 +1,43 @@
+package capitulo1;
+
+public class Ejercicio1punto2 {
+//	
+//	Antes que nada, descubrí cómo compilar y ejecutar un programa Java en tu
+//	entorno de desarrollo. Algunos entornos proveen programas de ejemplo similares
+//	al visto en la sección 1.5.
+//	
+//	a. Escribí el programa “Hola mundo”, luego compilalo y ejecutalo.
+	
+//	   public static void main (String[] args) {
+//		   System.out.println("Hola mundo");
+//	   }
+//	
+//	b. Agregá una segunda sentencia print que imprima otro mensaje después del
+//	“¡Hola mundo!”. Algo ingenioso como “Cómo estás?”. Compilá y ejecutá el
+//	programa de nuevo.
+	   
+//	   public static void main (String[] args) {
+//		   System.out.println("Hola mundo");
+//		   System.out.println("Cómo estás?");
+//		   
+//	   }
+//	
+//	c. Agregá una línea de comentario (en cualquier parte) y recompilá y ejecutá
+//	el programa nuevamente. La nueva línea no debería afectar la ejecución del
+//	mismo.
+	
+	
+	
+//	   Esta es una línea de comentario.
+//	
+	
+	
+//	Este ejercicio puede parecer trivial, pero es el punto de inicio para muchos de
+//	los programas con los que trabajaremos. Para poder depurar con confianza, debés
+//	tener seguridad sobre el entorno con el que programás. En algunos entornos, es
+//	fácil perder la noción de qué programa es el que se está ejecutando y podés encontrarte intentando depurar un programa mientras que accidentalmente, estás
+//	ejecutando otro. Agregar (y cambiar) impresiones en pantalla es una manera simple de establecer una conexión entre el programa que estás mirando y la salida
+//	que muestra cuando se ejecuta.
+
+
+}
