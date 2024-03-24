40072bb429faac941c4f3548292d221e87a89b1f
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-24 14:05:31 -0300

    Se ha agregado la carpeta capitulo2 y el ejercicio2.1

---
+++ Ejercicios libro - Pensando la computacion como un cientifico/capitulo2/capitulo2/src/capitulo2/Ejercicio2punto1.java
@@ -0,0 +1,95 @@
+package capitulo2;
+
+
+//Ejercicio 2.1
+//a. Creá un nuevo programa llamado Fecha.java. Copiá o tipeá algo como el
+//programa “Hola, Mundo” y asegurate de poder compilarlo y correrlo.
+//
+//		public class Fecha {
+//		    public static void main(String[] args) {
+//		        System.out.println("Hola, Mundo");
+//		    }
+//		}
+//
+//
+//b. Siguiendo el ejemplo en la Sección 2.4, escribí un programa que cree variables 
+//llamadas diaDeLaSemana, diaDelMes, mes y anio. dia contendrá el día
+//de la semana y diaDelMes contendrá el día del mes. ¿De qué tipo es cada
+//variable? Asigná valores a esas variables para que representen la fecha de
+//hoy.
+//
+//		public class Fecha {
+//		    public static void main(String[] args) {
+//		        String diaDeLaSemana = "Martes";
+//		        int diaDelMes = 24;
+//		        String mes = "Marzo";
+//		        int anio = 2024;
+//		    }
+//		}
+//
+//Las variables son:
+//
+//diaDeLaSemana: de tipo String, para almacenar el día de la semana.
+//diaDelMes: de tipo int, para almacenar el día del mes.
+//mes: de tipo String, para almacenar el nombre del mes.
+//anio: de tipo int, para almacenar el año.
+//
+//c. Imprimí el valor de cada variable en una línea distinta. Esto es un paso intermedio 
+//que sirve para chequear que todo está funcionando bien de momento.
+//d. Modificá el programa para que imprima la fecha en el estándar argentino
+//como: Lunes 9 de agosto de 2010 .
+//
+//		public class Fecha {
+//		    public static void main(String[] args) {
+//		        String diaDeLaSemana = "Martes";
+//		        int diaDelMes = 24;
+//		        String mes = "Marzo";
+//		        int anio = 2024;
+//		
+//		        System.out.println("Día de la semana: " + diaDeLaSemana);
+//		        System.out.println("Día del mes: " + diaDelMes);
+//		        System.out.println("Mes: " + mes);
+//		        System.out.println("Año: " + anio);
+//		    }
+//		}
+//
+//
+//e. Modificá el programa de nuevo para que ahora la salida total sea:
+//	
+//	public class Fecha {
+//    public static void main(String[] args) {
+//        String diaDeLaSemana = "Lunes";
+//        int diaDelMes = 9;
+//        String mes = "agosto";
+//        int anio = 2010;
+//
+//        // Formato argentino
+//        System.out.println("Formato argentino:");
+//        System.out.println(diaDeLaSemana + " " + diaDelMes + " de " + mes + " de " + anio);
+//
+//        // Formato americano
+//        System.out.println("\nFormato americano:");
+//        System.out.println(diaDeLaSemana + ", " + mes + " " + diaDelMes + ", " + anio);
+//
+//        // Formato europeo
+//        System.out.println("\nFormato europeo:");
+//        System.out.println(diaDeLaSemana + " " + diaDelMes + " " + mes + ", " + anio);
+//    }
+//}
+//
+//Este código imprimirá la fecha en los tres formatos especificados:
+//
+//Formato argentino: Lunes 9 de agosto de 2010
+//Formato americano: Lunes, agosto 9, 2010
+//Formato europeo: Lunes 9 agosto, 2010
+//Cada formato se imprime en líneas separadas para mayor claridad.
+//
+//
+//
+//
+//
+
+
+public class Ejercicio2punto1 {
+
+}
