17475395bd246437c95c144bcadcb79bff847d03
Pablo Domingo <pablodomingo1993@gmail.com>
2024-03-24 22:39:56 -0300

    Update Ejercicio5

--- Ejercicios para practicar - Strings/EjerciciosStrings/src/EjerciciosStrings/Ejercicio5.java
+++ Ejercicios para practicar - Strings/EjerciciosStrings/src/EjerciciosStrings/Ejercicio5.java
@@ -1,10 +1,18 @@
 package EjerciciosStrings;
 
+//5. Implementar la función 
+//public static boolean esPrefijo(String prefijo, String s) 
+//que consulta si el string prefijo 
+//coincide con los primeros caracteres de s.
+
 public class Ejercicio5 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static boolean esPrefijo(String prefijo, String s) {
+		for(int i=0; i<prefijo.length();i++) {
+			if(prefijo.charAt(i)!=s.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
 	}
-
 }
