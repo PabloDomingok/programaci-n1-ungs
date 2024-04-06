0bc0789d890270f764561a249799c07ba94e90bb
Pablo Domingo <pablodomingo1993@gmail.com>
2024-04-06 12:55:31 -0300

    Se ha agregado el ejercicio3.2 de los ejercicios del libro

---
+++ Ejercicios libro - Pensando la computacion como un cientifico/capitulo3/capitulo3/src/capitulo3/Ejercicio3_3.java
@@ -0,0 +1,36 @@
+package capitulo3;
+
+public class Ejercicio3_3 {
+	
+//	El objetivo de este ejercicio es asegurarte de que entiendas cómo escribir 
+//	y llamar métodos que toman parámetros.
+//
+//	a. Escribir la primera línea de un método llamado zool que toma tres 
+//	parámetros: un int y dos Strings.
+//
+//	b. Escribir una línea de código que invoque a zool, pasando como 
+//	argumentos al valor 11, el nombre de tu primera mascota y 
+//	el nombre de la calle en la cual creciste.
+	
+//	a. Primera línea del método zool:
+		
+		public static void zool(int numero, String mascota, String calle) {
+		
+	}
+		
+//	b. Línea de código para invocar zool con los argumentos especificados:
+		
+		public static void main (String []args) {
+			zool(11, "NombreDeTuMascota", "NombreDeTuCalle");
+
+		}
+//		Explicación:
+//
+//			zool es el nombre del método.
+//			Toma tres parámetros: un entero (int) llamado numero y dos 
+//			cadenas (String) llamadas mascota y calle.
+//			En la línea de código de invocación, se pasan los valores 
+//			11, el nombre de tu primera mascota y el nombre de la calle en 
+//			la cual creciste como argumentos.
+
+}
